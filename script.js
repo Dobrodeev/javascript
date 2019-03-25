a8638ee5f0498df4297dd8fa58ddce67d2f3712b
Dobrodeev <dobrod@meta.ua>
2019-03-25 21:45:47 +0200

    перенесли функцию в отдельный файл - script.js

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ function myfunc() {
     document.getElementById('demo').innerHTML='Zi zi top';
 
 }
+function textFunction()
+{
+    document.getElementById("someID").innerHTML = 'Some text for someID';
+}
