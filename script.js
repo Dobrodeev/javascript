bde66f46da07fdef820ac564a16804d30431ec10
Dobrodeev <dobrod@meta.ua>
2019-03-25 21:53:55 +0200

    перенесли функцию в отдельный файл script.js. Функция вызываеться по нажатию на кнопку button

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ function textFunction()
 {
     document.getElementById("someID").innerHTML = 'Some text for someID';
 }
+
+function f() {
+    var x, y, z;
+    x = 5;
+    y = 6;
+    z = x + y;
+    document.write('x=',x);
+    document.write('y=',y);
+    document.write('x+y=', z);
+    document.write('/n');
+    for (i = 0; i < 10; i++)
+    {
+        document.writeln(i);
+    }
+}
