86337ab4d337a78bae955198d784aa2fc27bd59e
Dobrodeev <dobrod@meta.ua>
2019-04-01 22:54:28 +0300

    в скрипте script.js создали две функции: которые выводят игральную карту и ее масть

--- script.js
+++ script.js
@@ -1,8 +1,52 @@
 function myfunc() {
     // document.getElementById('demo').innerHTML='hell';
 
-    document.getElementById('demo').innerHTML='Zi zi top';
-
+    // document.getElementById('demo').innerHTML='Zi zi top';
+    var card = prompt('Назвите кату от 2 до Туза: 2,3,4,...,J, Q, K, A', 0);
+    var result;
+    switch (card) {
+        case 2: result = 2;
+            break;
+        case 3: result = 3;
+            break;
+        case 4: result = 2;
+            break;
+        case 5: result = 3;
+            break;
+        case 6: result = 2;
+            break;
+        case 7: result = 3;
+            break;
+        case 8: result = 2;
+            break;
+        case 9: result = 3;
+            break;
+        case 10:
+        case 'J':
+        case 'Q':
+        case 'K': result = 10;
+        break;
+        case 'A': result = 11;
+        break;
+        default : result = 'Неясно что.'
+    }
+    alert(result);
+}
+function suitOfCard() {
+    var variant = +prompt('Масть карты - число от 0 до 3', 0)
+    var suit;
+    switch (variant) {
+        case 0 : suit = 'clubs - трефы';
+        break;
+        case 1: suit = 'hearts';
+        break;
+        case 2: suit = 'diamonds';
+        break;
+        case 3: suit = 'spades - пики';
+        break;
+        default: suit = 'Непонятно какая масть.'
+    }
+    alert(suit)
 }
 function textFunction()
 {
---
+++ script2.js
@@ -0,0 +1,19 @@
+function seasonsOfYear() {
+    /*var num, season;
+    num = +prompt('Число от 1 до 4. Будет выводится пора года: зима, весна, лето, осень.');
+    switch (num)
+    {
+        case 1: season='зима';
+            break;
+        case 2: season = 'весна';
+            break;
+        case 3 : season = 'summer';
+            break;
+        case 4: season = 'winter';
+            break;
+        default: season = '';
+    }*/
+    // alert(season);
+    alert('This is alert.')
+    document.getElementById('season').innerHTML='show season'
+}
