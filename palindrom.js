7608a42672aeaf9112374a6b4dd1f543555cabb9
Dobrodeev <dobrod@meta.ua>
2019-06-13 15:29:31 +0300

    функция f() определяет является ли слово палиндромом, например: мадам, кок

---
+++ palindrom.js
@@ -0,0 +1,15 @@
+function f() {
+    var word, wordArray;
+    word = document.getElementById('number').value;
+    wordArray = word.split('');
+    wordArrayReverse = word.split('').reverse();
+    for (var i = 0; i < wordArray.length; i ++){
+        if (wordArray[i] !== wordArrayReverse[i]){
+            alert('Не палиндром');
+            return;
+        }
+    }
+    alert('Palindrom');
+    return;
+}
+
