0a5b7c59fa7eefe75ac230f573792ba39bc59fe4
Dobrodeev <dobrod@meta.ua>
2019-06-13 15:40:45 +0300

    создали елемент <li> с помощью createElement() и вставили его в DOM с помощью метода appendChild(li)

---
+++ lessonAtributes3.js
@@ -0,0 +1,25 @@
+/*
+let div = document.getElementById('elem');
+div.classList.add('wwww');
+
+div.classList.remove('elem');
+let res = div.classList.contains('elem');
+
+/!*for (let i = 0; i < classNames.length; i ++){
+    alert(classNames[i]);
+}*!/
+div.style.cssText = 'color:red; font-size:30px; border:1px solid #ccc';
+div.addEventListener('click',tag);
+function tag() {
+    this.innerHTML = div.tagName;
+}
+let ol = document.getElementById('ol');
+let li = document.createElement('li');
+li.innerHTML = 'Пункт';
+ol.appendChild(li);
+let arr = ['Буква', 'погода', 'природа'];
+for (let i = 0; i < arr.length; i ++) {
+    let li = document.createElement('li');
+    li.innerHTML = arr[i];
+    ol.appendChild(li);
+}*/
