e6bd787886fd4e5bda79b95bd80ad04d0a8d5999
Dobrodeev <dobrod@meta.ua>
2019-06-13 23:42:04 +0300

    реализовали асинхронное добавление строк в таблицу с помощью Ajax

---
+++ assets/ajax.php
@@ -0,0 +1,6 @@
+<?php
+$rel = addslashes($_REQUEST['rel']);
+?>
+<tr><td><?php echo 4040 + $rel;?></td><td><?php echo 4040 + $rel;?></td><td><?php echo 4040 + $rel;?></td></tr>
+    <tr><td><?php 12000 + $rel;?></td><td><?php 12000 + $rel;?></td><td><?php 12000 + $rel;?></td></tr>
+    <tr><td><? echo 24000 + $rel;?></td><td><? echo 24000 + $rel;?></td><td><? echo 24000 + $rel;?></td></tr>
