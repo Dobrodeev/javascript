8b0dd402361dd96066b5b4e5b8df5f19b4f0977c
Dobrodeev <dobrod@meta.ua>
2019-07-27 14:25:07 +0300

    вывели кафедры и названия групп кафедр ИС, ИК, ИА, ИО

--- Classes/Tree.php
+++ Classes/Tree.php
@@ -9,19 +9,20 @@
 class Tree
 {
     private $pdo;
+    
     public function __construct()
     {
         $host = '127.0.0.1';
-        $db   = 'regandauto';
+        $db = 'regandauto';
         $user = 'root';
         $pass = '';
         $charset = 'utf8';
 
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
         $opt = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_EMULATE_PREPARES => false,
         ];
         $this->pdo = new PDO($dsn, $user, $pass, $opt);
     }
@@ -29,11 +30,11 @@ class Tree
     public function getFromTable()
     {
         $stmt = $this->pdo->query('SELECT * FROM test_cafedra');
-        while ($row = $stmt->fetch())
-        {
-            echo $row['name_cafedra'] . "\n";
+        while ($row = $stmt->fetch()) {
+            echo $row['name_cafedra'] . ' ' . $row['groups_cafedra'] . '<br>';
         }
     }
+
     public function connectDB()
     {
         /*$host = "localhost";
@@ -49,16 +50,16 @@ class Tree
         ];*/
 //        $PDO = new PDO("mysql:host=localhost;dbname=$dbname", "$login", "$dbpass", $opt);
         $host = '127.0.0.1';
-        $db   = 'regandauto';
+        $db = 'regandauto';
         $user = 'root';
         $pass = '';
         $charset = 'utf8';
 
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
         $opt = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_EMULATE_PREPARES => false,
         ];
         $pdo = new PDO($dsn, $user, $pass, $opt);
         $rows = array();
