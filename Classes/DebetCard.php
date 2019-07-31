af1491ac6cb99fa7a42c12784d7fb9753b2445c5
Dobrodeev <dobrod@meta.ua>
2019-07-31 14:20:55 +0300

    добавили проверку если карта начинееться не с 37, то false

--- Classes/DebetCard.php
+++ Classes/DebetCard.php
@@ -32,30 +32,35 @@ class DebetCard
         $firstNumber = [4, 5, 37, 6];
         $str_split = str_split($card);
         $getFirst = $str_split[0];
-        $doubleDown = 0;
-        $oddSum = 0;
-        if (in_array($getFirst, $firstNumber)) {
-            for ($i = count($str_split) - 1; $i >= 0; $i--) {
-                if ($i % 2 == 0) {
-                    $str_split[$i] = $str_split[$i] * 2;
-                    if ($str_split > 9) {
-                        $first = (int)($str_split[$i] / 10);
-                        $second = $str_split[$i] % 10;
-                        $str_split[$i] = $first + $second;
-                        $doubleDown += $str_split[$i];
-                    }
-                } else {
-                    $oddSum += $str_split[$i];
-                }
-            }
-            $amount = $doubleDown + $oddSum;
-            if ($amount % 10 == 0)
-                echo 'Card is valid. <br>';
-            else
-                echo 'Card is not valid. <br>';
-            echo '$doubleDown = ' . $doubleDown . '<br>';
-            echo '$oddSum = ' . $oddSum . '<br>';
-            echo '$amount = ' . $amount . '<br>';
+        /*$doubleDown = 0;
+        $oddSum = 0;*/
+        $amount = 0;
+        if (!in_array($getFirst, $firstNumber) || $str_split[0] == 3 && $str_split[1] != 7) {
+//            echo 'Card is not valid. <br>';
+            return false;
         }
+        for ($i = count($str_split) - 1; $i >= 0; $i--) {
+            if ($i % 2 == 0) {
+                $str_split[$i] = $str_split[$i] * 2;
+                if ($str_split[$i] > 9) {
+//                        $first = (int)($str_split[$i] / 10);
+//                        $second = $str_split[$i] % 10;
+//                        $str_split[$i] = 1 + $str_split[$i] % 10;
+                    $amount += 1 + $str_split[$i] % 10;
+                }
+            } else {
+                $amount += $str_split[$i];
+            }
+        }
+//            $amount = $doubleDown + $oddSum;
+        if ($amount % 10 == 0)
+//            echo 'Card is valid. <br>';
+            return true;
+        else
+//            echo 'Card is not valid. <br>';
+            return false;
+        /*echo '$doubleDown = ' . $doubleDown . '<br>';
+        echo '$oddSum = ' . $oddSum . '<br>';
+        echo '$amount = ' . $amount . '<br>';*/
     }
 }
